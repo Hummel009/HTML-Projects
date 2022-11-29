900bd3515c46a0b69190eaadd1a27f52724dc4de
Hummel009 <emperorbur@gmail.com>
2022-11-29 12:50:44 +0300

    names

--- Hundom/src/main/webapp/amogus.js
+++ Hundom/src/main/webapp/amogus.js
@@ -12,16 +12,16 @@ function loader() {
 	loadIfExists("also");
 	loadIfExists("day");
 	loadIfExists("intro");
-	loadIfExists("licenseid");
+	loadIfExists("license");
 	loadIfExists("list");
 	loadIfExists("more");
 	loadIfExists("support");
 	loadIfExists("title");
 	loadIfExists("workers");
 	loadIfExists("langs");
-	loadIfExists("list2");
-	loadIfExists("searchrc");
-	loadIfExists("znaist");
+	loadIfExists("quantity");
+	loadIfExists("search");
+	loadIfExists("find");
 }
 
 function toEng() {
@@ -29,7 +29,7 @@ function toEng() {
 		localStorage.setItem("also", "See also");
 		localStorage.setItem("day", "Person of the day");
 		localStorage.setItem("intro", "Welcome, visitor (please join us) to this internet portal where you can find information about some Belarusian writers. Read and learn new things!");
-		localStorage.setItem("licenseid", "Community materials are available under the terms of the CC-BY-SA license, unless otherwise specified.");
+		localStorage.setItem("license", "Community materials are available under the terms of the CC-BY-SA license, unless otherwise specified.");
 		localStorage.setItem("list", "List of people");
 		localStorage.setItem("more", "Read more");
 		localStorage.setItem("support", "For technical support, please contact the BSUIR FCSIS PSIT frontenders team.");
@@ -37,9 +37,9 @@ function toEng() {
 		localStorage.setItem("workers", "Dungeon Masters");
 
 		localStorage.setItem("langs", "Choose a language");
-		localStorage.setItem("list2", "List of people");
-		localStorage.setItem("searchrc", "Search");
-		localStorage.setItem("znaist", "Find");
+		localStorage.setItem("quantity", "List of people");
+		localStorage.setItem("search", "Search");
+		localStorage.setItem("find", "Find");
 
 		loader();
 	} else {
@@ -52,17 +52,17 @@ function toBel() {
 		localStorage.setItem("also", "Глядзіце таксама");
 		localStorage.setItem("day", "Дзеяч дня");
 		localStorage.setItem("intro", "Сардэчна запрашаем, наведвальнік (калі ласка, далучайся да нас) на гэты інтэрнэт-партал, дзе вы можаце знайсці інфармацыю аб некаторых беларускіх пісьменніках. Чытайце і пазнавайце новае!");
-		localStorage.setItem("licenseid", "Матэрыялы супольнасці даступныя ўадпаведнасці з умовамі ліцэнзіі CC-BY-SA, калі не пазначана іншае.");
+		localStorage.setItem("license", "Матэрыялы супольнасці даступныя ўадпаведнасці з умовамі ліцэнзіі CC-BY-SA, калі не пазначана іншае.");
 		localStorage.setItem("list", "Спіс дзеячаў");
 		localStorage.setItem("more", "Чытаць падрабязней");
 		localStorage.setItem("support", "Па пытаннях тэхнічнай падтрымкі звяртацца да каманды франтэндэраў БДУІР ФКСіС ПЗІТ.");
 		localStorage.setItem("title", "Галоўная");
 		localStorage.setItem("workers", "Распрацоўшчыкі");
 
-		localStorage.setItem("list2", "Спіс дзеячаў");
+		localStorage.setItem("quantity", "Спіс дзеячаў");
 		localStorage.setItem("langs", "Перакласці");
-		localStorage.setItem("searchrc", "Пошук");
-		localStorage.setItem("znaist", "Знайсці");
+		localStorage.setItem("search", "Пошук");
+		localStorage.setItem("find", "Знайсці");
 
 		loader();
 	} else {
